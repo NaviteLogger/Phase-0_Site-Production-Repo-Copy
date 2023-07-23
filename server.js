fac39a57a0d7b949752b271e451daebfa4d94982
NaviteLogger <kacprzakmarek92@gmail.com>
2023-07-23 18:43:56 +0200

    Changed the name of the sql table

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/login', function(req, res) {
 
   console.log('Email: ' + email);
 
-  connection.query('SELECT * FROM users WHERE username = ?', [email], function (error, results, fields) {
+  connection.query('SELECT * FROM Clients WHERE username = ?', [email], function (error, results, fields) {
     if (error) {
       res.status(500).json({ error: 'Internal Server Error', error});
       return;
