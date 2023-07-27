7917f04445c8ad805f0e59d223774523c785f353
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-07-27 15:52:26 +0200

    Fixed a route typo in another file

--- scripts/ClientsPortalPageScript.js
+++ scripts/ClientsPortalPageScript.js
@@ -1,17 +1,17 @@
 document.getElementById('clients-portal').addEventListener('click', function() {
 
-    fetch('/clientsPortalProtected', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        .then(response => response.json())
-        .then(data => {
-          if (data.status === 'not_logged_in') 
-          {
-            window.location.href = '/pages/LoginPage.html';
-          }
-        })
-        .catch(error => console.error(error));
+  fetch('/pages/ClientsPortalPage.html', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'not_logged_in') 
+        {
+          window.location.href = '/pages/LoginPage.html';
+        }
+      })
+      .catch(error => console.error(error));
 });
