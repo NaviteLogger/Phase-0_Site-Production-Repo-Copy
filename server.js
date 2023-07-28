c1a1a3bef46efe289ea1b4b4cfba9b6297a620c4
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-07-28 16:27:48 +0200

    Added a test email sending using sendgrid

--- server.js
+++ server.js
@@ -73,8 +73,8 @@ app.use(flash());
 
 //Send a test mail using sendgrid:
 const msg = {
-  to: 'test@example.com', // Change to your recipient
-  from: 'test@example.com', // Change to your verified sender
+  to: 'kacprzakmarek077@gmail.com', // Change to your recipient
+  from: 'kacprzakmarek92@gmail.com', // Change to your verified sender
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
