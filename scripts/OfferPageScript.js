25e7c6b97a04d70490b8c9218101dde51a81d016
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-08-01 14:50:30 +0200

    Added listeners to all the buttons on the offerPage

--- scripts/OfferPageScript.js
+++ scripts/OfferPageScript.js
@@ -1,23 +1,29 @@
-document.getElementById('clients-portal').addEventListener('click', function() {
+//Include all the buttons we want to attach event listeners to
+const buttonsIds = ['clients-portal', 'clients-portal-subscription', 'clients-portal-list'];
 
-  fetch('/checkIfAuthenticated', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.status == 'logged_in')
-        {
-          window.location.href = '/clientsPortalPage';
-        }
-        else 
-        {
-          window.location.href = '/pages/NotLoggedInPage.html';
-        }
-      })
-      .catch((error) => {
-        console.error(error)
-      });
+//Add event listeners to all the buttons
+buttonsIds.forEach((buttonId) => {
+  document.getElementById(buttonId).addEventListener('click', function() {
+
+    fetch('/checkIfAuthenticated', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.status == 'logged_in')
+          {
+            window.location.href = '/clientsPortalPage';
+          }
+          else 
+          {
+            window.location.href = '/pages/NotLoggedInPage.html';
+          }
+        })
+        .catch((error) => {
+          console.error(error)
+        });
+  });
 });
