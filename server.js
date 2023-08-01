d65eca865ec3be1a28201099cf787ff2e56e35b4
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-08-01 12:37:48 +0200

    Modified the lookup of OfferPage.ejs

--- server.js
+++ server.js
@@ -546,7 +546,7 @@ app.get('/offerPage', (req, res) => {
     }
       else
     {
-      res.render('/views/OfferPage.ejs', { files: files });
+      res.render('OfferPage', { files: files });
     }
   });
 });
