e8994be5ed5b6de42937f2a9848e9379b3789a6d
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-08-09 19:20:34 +0200

    Added a fix with regard to two separate constants PDFDocument

--- server.js
+++ server.js
@@ -645,7 +645,7 @@ app.get('/RODOAgreementImage/:index', checkAuthentication, checkEmailConfirmatio
 app.post('/submitAllSignedRODOAgreements', async (req, res) => {
   try {
       const images = req.body.images; // Assuming images is an array of dataURLs sent from the client.
-      const doc = new PDFDocument();
+      const doc = new PDFKitDocument();
       const outputPDFPath = path.join(__dirname, 'path_to_save_signed_pdfs', 'output.pdf'); // Modify this path as necessary
       const stream = fs.createWriteStream(outputPDFPath);
       
