376dfa48cb34c183b23f2b17cbf1fd62c923826f
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-08-13 22:41:23 +0200

    Adjusted the InterviewPageScript to handle the 3 question categories

--- scripts/InterviewPageScript.js
+++ scripts/InterviewPageScript.js
@@ -9,12 +9,25 @@ document.addEventListener('DOMContentLoaded', function() {
         radio.addEventListener('change', function() {
             const questionContainer = this.closest('.question');
             const questionLabel = questionContainer.querySelector('.question-label');
+            const textarea = questionContainer.querySelector('textarea');
 
             if (questionContainer.classList.contains('tf-category')) {
                 if (this.value === 'true') {
                     questionLabel.style.color = 'red';
                 } else if (this.value === 'false') {
                     questionLabel.style.color = 'green';
+                }
+            } else if (questionContainer.classList.contains('tf-explanation-category')) {
+                if (this.value === 'true') {
+                    questionLabel.style.color = 'yellow';
+                    if (textarea) {
+                        textarea.disabled = false;
+                    }
+                } else if (this.value === 'false') {
+                    questionLabel.style.color = 'green';
+                    if (textarea) {
+                        textarea.disabled = true;
+                    }
                 }
             }
         });
