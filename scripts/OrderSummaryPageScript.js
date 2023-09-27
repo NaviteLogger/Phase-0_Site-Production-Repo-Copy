0d58c4d601bddc8cec26e085fe83e5913480295c
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-09-27 20:11:43 +0200

    Added an event listener for the payment button

--- scripts/OrderSummaryPageScript.js
+++ scripts/OrderSummaryPageScript.js
@@ -1,4 +1,7 @@
 document.getElementById('clients-portal').addEventListener('click', () => {
     window.location.href = '/clientsPortalPage';
   });
+
+document.getElementById('payment-button').addEventListener('click', () => {
   
+});
