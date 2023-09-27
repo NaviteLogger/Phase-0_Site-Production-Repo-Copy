6a48d59e460c84d2eb1f30589815409dc01fcf8b
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-09-27 21:38:28 +0200

    Modified the OrderSummaryPageScript to handle the POST request to /makePaymentsForAgreements

--- scripts/OrderSummaryPageScript.js
+++ scripts/OrderSummaryPageScript.js
@@ -1,8 +1,34 @@
-document.getElementById('clients-portal').addEventListener('click', () => {
-    window.location.href = '/clientsPortalPage';
-  });
+document.getElementById("clients-portal").addEventListener("click", () => {
+  window.location.href = "/clientsPortalPage";
+});
 
-document.getElementById('payment-button').addEventListener('click', () => {
+document.getElementById("payment-button").addEventListener("click", () => {
+  const selectedAgreementsNames = document.body.getAttribute(
+    "selectedAgreementsNames"
+  );
+  const selectedAgreementsPrices = document.body.getAttribute(
+    "selectedAgreementsPrices"
+  );
+
   //The payment is processed by the server for security reasons
-  window.location.href = '/makePaymentForTheAgreements';
+  fetch("/makePaymentsForAgreements", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      selectedAgreementsNames: selectedAgreementsNames,
+      selectedAgreementsPrices: selectedAgreementsPrices,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === 'success') {
+        setTimeout(() => {
+          window.location.href = "..."; // Redirect to the choice
+        }, 1000);
+      } else {
+        console.error("Failed to process payment.");
+      }
+    });
 });
