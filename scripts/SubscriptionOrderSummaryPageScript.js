78e6a514658c0c42540f7864f4780f3443e11044
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-03 21:08:23 +0200

    Added a list of availabe agreements for the user to choose while buying the subscription

--- scripts/SubscriptionOrderSummaryPageScript.js
+++ scripts/SubscriptionOrderSummaryPageScript.js
@@ -1,4 +1,45 @@
 document.getElementById('clients-portal').addEventListener('click', () => {
     window.location.href = '/clientsPortalPage';
   });
-  
+
+  document
+  .getElementById("buy-selected-subscription")
+  .addEventListener("click", () => {
+    const form = document.getElementById("buy-selected-agreements-form"); //Get the form
+
+    const checkboxes = document.querySelectorAll(
+      '#buy-selected-subscription-form input[type="checkbox"][name="agreement"]'
+    );
+
+    const selectedAgreements = [];
+
+    checkboxes.forEach((checkbox) => {
+      //Check if the checkbox is checked
+      if (checkbox.checked) {
+        //If checked, add its value (agreement_id) to the form
+        selectedAgreements.push(checkbox.value);
+      }
+    });
+
+    console.log(selectedAgreements);
+
+    fetch("/buySelectedAgreements", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(selectedAgreements), //Send the selected agreements to the server
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "success") {
+          window.location.href = "/orderSummaryPage";
+        }
+      })
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      });
+  });
